590814736075aeaebe9c6a8d775fe955c6e10b95
anaurelian <dev@anaurelian.com>
2021-12-27 17:25:11 +0200

    Add footer; document JS code

--- page/js/app.js
+++ page/js/app.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021 anaurelian. All rights reserved.
+// Copyright 2020-2021 anaurelian. All rights reserved.
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
 
@@ -17,70 +17,88 @@ class App {
   #delayEl;
 
   /**
-   * Init the application.
+   * Initializes the application object.
    */
   constructor() {
+    // Cache some frequently used DOM elements
     this.#createdCounterEl = document.querySelector('#created-counter');
     this.#clickedCounterEl = document.querySelector('#clicked-counter');
     this.#remainingCounterEl = document.querySelector('#remaining-counter');
     this.#delayEl = document.querySelector('#delay');
 
+    // Initialize slower/faster buttons
     document.querySelector('#slower-button').addEventListener('click', (e) => {
       this.#delay += 100;
       this.#updateDelay();
     });
-
     document.querySelector('#faster-button').addEventListener('click', (e) => {
       if (this.#delay > 100) this.#delay -= 100;
       this.#updateDelay();
     });
 
+    // Start by adding the first button
     this.#addButton();
   }
 
+  /**
+   * Updates the counters.
+   */
   #updateCounters() {
     this.#createdCounterEl.innerText = this.#createdCounter;
     this.#clickedCounterEl.innerText = this.#clickedCounter;
     this.#remainingCounterEl.innerText = this.#createdCounter - this.#clickedCounter;
   }
 
+  /**
+   * Updates the delay value.
+   */
   #updateDelay() {
     this.#delayEl.innerText = this.#delay;
   }
 
+  /**
+   * Adds a new random color button.
+   */
   #addButton() {
     const button = document.createElement('button');
     button.classList.add('absolute', 'color-button');
 
+    // Set a random button color
     const color = getRandomColor();
     button.innerText = color.backcolor;
     button.style.backgroundColor = color.backcolor;
     button.style.color = color.textcolor;
-    button.style.zIndex = 0;
 
+    // Initialize the button click event
     button.addEventListener('click', (e) => {
-      this.#clickedCounter++;
-      this.#updateCounters();
-      
+      // Update the background and text color of the document body
       document.body.style.backgroundColor = e.target.style.backgroundColor;
       document.body.style.color = e.target.style.color;
   
+      // Remove the button
       e.target.remove();
+
+      // Update the clicked counter
+      this.#clickedCounter++;
+      this.#updateCounters();
     });
 
+    // Add the button at a random position
     document.body.appendChild(button);
     const position = getRandomPosition(button);
     button.style.left = position.left + 'px';
     button.style.top = position.top + 'px';
 
+    // Update the created counter
     this.#createdCounter++;
     this.#updateCounters();
 
+    // Schedule the next button add
     setTimeout(() => this.#addButton(), this.#delay);
   }
 }
 
 /**
- * Init the application on window load event.
+ * Initialize the application in the window load event.
  */
  window.addEventListener("load", () => new App());
--- page/js/utils.js
+++ page/js/utils.js
@@ -1,5 +1,12 @@
+// Copyright 2020-2021 anaurelian. All rights reserved.
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file.
+
 'use strict';
 
+/**
+ * Generates a random position for the specified element in the bounds of its parent.
+ */
 function getRandomPosition(child) {
   const childRect = child.getBoundingClientRect();
   const parentRect = child.parentNode.getBoundingClientRect();
@@ -8,12 +15,19 @@ function getRandomPosition(child) {
   return { left: left, top: top };
 }
 
+/**
+ * Generates a random background color and its associated contrast text color.
+ */
 function getRandomColor() {
   const hexcolor = `${Math.random().toString(16).slice(-6)}`;
   const contrastColor = getContrastYIQ(hexcolor);
   return { backcolor: `#${hexcolor}`, textcolor: contrastColor };
 }
 
+/**
+ * Calculates the contrast color of the specified hex color.
+ * From https://24ways.org/2010/calculating-color-contrast/
+ */
 function getContrastYIQ(hexcolor) {
   var r = parseInt(hexcolor.substr(0, 2), 16);
   var g = parseInt(hexcolor.substr(2, 2), 16);
