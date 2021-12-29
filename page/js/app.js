3879fc33c16693a0ffae5909986e32cb80f8e098
anaurelian <dev@anaurelian.com>
2021-12-29 15:11:37 +0200

    Refactor JS code without classes

--- page/js/app.js
+++ page/js/app.js
@@ -6,136 +6,143 @@
 
 import { getRandomPosition, getRandomColor } from '/js/utils.js';
 
-class App {
 
-  #delay = 1000;
-  #winOnNoRemains = true;
-  #createdCounter = 0;
-  #clickedCounter = 0;
-  #prevClickedCounter = 0;
 
-  #maxClickrate = 0;
+let delay = 1000;
+let winOnNoRemains = true;
+let createdCounter = 0;
+let clickedCounter = 0;
+let prevClickedCounter = 0;
 
-  #startTime;
-  #prevTime;
+let maxClickrate = 0;
 
-  /**
-   * Cached DOM elements
-   */
-  #createdCounterEl;
-  #clickedCounterEl;
-  #remainingCounterEl;
-  #clickrateStatsEl;
+let startTime;
+let prevTime;
 
-  #addButtonIntervalID;
+/**
+ * Cached DOM elements
+ */
+let clickrateStatsEl;
 
-  /**
-   * Initializes the application object.
-   */
-  constructor() {
-    // Cache some frequently used DOM elements
-    this.#createdCounterEl = document.querySelector('#created-counter');
-    this.#clickedCounterEl = document.querySelector('#clicked-counter');
-    this.#remainingCounterEl = document.querySelector('#remaining-counter');
-    this.#clickrateStatsEl = document.querySelector('#clickrate-stats');
+let addButtonIntervalID;
 
-    document.querySelector('#config-start').addEventListener('click', (e) => this.#start());
-  }
+/**
+ * Initializes the application object.
+ */
+function initPage() {
+  // Cache some frequently used DOM elements
 
-  #start() {
-    document.querySelector('#config-section').classList.add('hidden');
-    document.querySelector('main').classList.remove('hidden');
+  clickrateStatsEl = document.querySelector('#clickrate-stats');
 
-    // Add initial buttons
-    this.#addInitialButtons();
+  document.querySelector('#config-start').addEventListener('click', (e) => start());
+}
 
-    this.#delay = document.querySelector('#config-delay').value;
-    this.#winOnNoRemains = document.querySelector('#config-win-on-no-remains').checked;
+function start() {
+  document.querySelector('#config-section').classList.add('hidden');
+  document.querySelector('main').classList.remove('hidden');
 
-    // Start adding buttons
-    this.#addButtonIntervalID = setInterval(() => this.#addButton(), this.#delay);
+  // Add initial buttons
+  addInitialButtons();
 
-    this.#prevTime = Date.now();
-    setInterval(() => {
-      const clickrate = (this.#clickedCounter - this.#prevClickedCounter) * 1000 / (Date.now() - this.#prevTime);
-      
-      this.#prevClickedCounter = this.#clickedCounter;
-      this.#prevTime = Date.now();
+  delay = document.querySelector('#config-delay').value;
+  winOnNoRemains = document.querySelector('#config-win-on-no-remains').checked;
 
-      this.#maxClickrate = Math.max(this.#maxClickrate, clickrate);
+  // Start adding buttons
+  addButtonIntervalID = setInterval(() => addButton(), delay);
 
-      this.#clickrateStatsEl.textContent = `${clickrate.toFixed(2)} / ${this.#maxClickrate.toFixed(2)}`;
-    }, 1000);
-  }
+  prevTime = Date.now();
+  setInterval(() => {
+    const clickrate = (clickedCounter - prevClickedCounter) * 1000 / (Date.now() - prevTime);
+    
+    prevClickedCounter = clickedCounter;
+    prevTime = Date.now();
 
-  #addInitialButtons() {
-    const initialCount = document.querySelector('#config-initial').value;
-    for (let i = 0; i < initialCount; i++) {
-      setTimeout(() => this.#addButton());
-    }
-  }
+    maxClickrate = Math.max(maxClickrate, clickrate);
 
-  /**
-   * Updates the counters.
-   */
-  #updateCounters() {
-    const remainingCounter = this.#createdCounter - this.#clickedCounter;
+    clickrateStatsEl.textContent = `${clickrate.toFixed(2)} / ${maxClickrate.toFixed(2)}`;
+  }, 1000);
+}
 
-    this.#createdCounterEl.innerText = this.#createdCounter;
-    this.#clickedCounterEl.innerText = this.#clickedCounter;
-    this.#remainingCounterEl.innerText = remainingCounter;
+/**
+ * Adds a new random color button.
+ */
+function addButton() {
+  const button = document.createElement('button');
+  button.classList.add('absolute', 'color-button');
 
-    if (this.#winOnNoRemains && (remainingCounter == 0)) {
-      this.#showWonScreen();
-      clearInterval(this.#addButtonIntervalID);
-    }
-    // document.title = `${this.#createdCounter} created, ${this.#clickedCounter} clicked, ${this.#createdCounter - this.#clickedCounter} remaining`;
-  }
+  // Set a random button color
+  const color = getRandomColor();
+  button.innerText = color.backcolor;
+  button.style.backgroundColor = color.backcolor;
+  button.style.color = color.textcolor;
 
-  /**
-   * Adds a new random color button.
-   */
-  #addButton() {
-    const button = document.createElement('button');
-    button.classList.add('absolute', 'color-button');
+  // Initialize the button click event
+  button.addEventListener('click', (e) => {
+    // Update the background and text color of the document body
+    document.body.style.backgroundColor = e.target.style.backgroundColor;
+    document.body.style.color = e.target.style.color;
 
-    // Set a random button color
-    const color = getRandomColor();
-    button.innerText = color.backcolor;
-    button.style.backgroundColor = color.backcolor;
-    button.style.color = color.textcolor;
+    // Remove the button
+    e.target.remove();
 
-    // Initialize the button click event
-    button.addEventListener('click', (e) => {
-      // Update the background and text color of the document body
-      document.body.style.backgroundColor = e.target.style.backgroundColor;
-      document.body.style.color = e.target.style.color;
-  
-      // Remove the button
-      e.target.remove();
+    // Update the clicked counter
+    clickedCounter++;
+    updateCounters();
+  });
 
-      // Update the clicked counter
-      this.#clickedCounter++;
-      this.#updateCounters();
-    });
+  // Add the button at a random position
+  document.body.appendChild(button);
+  const position = getRandomPosition(button);
+  button.style.left = position.left + 'px';
+  button.style.top = position.top + 'px';
 
-    // Add the button at a random position
-    document.body.appendChild(button);
-    const position = getRandomPosition(button);
-    button.style.left = position.left + 'px';
-    button.style.top = position.top + 'px';
+  // Update the created counter
+  createdCounter++;
+  updateCounters();
+}
 
-    // Update the created counter
-    this.#createdCounter++;
-    this.#updateCounters();
-  }
-
-  #showWonScreen() {
-    document.querySelector('#won-section').classList.remove('hidden');
+function addInitialButtons() {
+  const initialCount = document.querySelector('#config-initial').value;
+  for (let i = 0; i < initialCount; i++) {
+    setTimeout(() => addButton());
   }
 }
 
 /**
+ * Updates the counters.
+ */
+function updateCounters() {
+
+  if (typeof updateCounters.createdEl === "undefined") {
+    updateCounters.createdEl = document.querySelector('#stats-created');
+  }
+  if (typeof updateCounters.clickedEl === "undefined") {
+    updateCounters.clickedEl = document.querySelector('#stats-clicked');
+  }
+  if (typeof updateCounters.remainingEl === "undefined") {
+    updateCounters.remainingEl = document.querySelector('#stats-remaining');
+  }
+
+  const remainingCounter = createdCounter - clickedCounter;
+
+  updateCounters.createdEl.textContent = createdCounter;
+  updateCounters.clickedEl.textContent = clickedCounter;
+  updateCounters.remainingEl.textContent = remainingCounter;
+
+  if (winOnNoRemains && (remainingCounter == 0)) {
+    showWinScreen();
+    clearInterval(addButtonIntervalID);
+  }
+  // document.title = `${createdCounter} created, ${clickedCounter} clicked, ${createdCounter - clickedCounter} remaining`;
+}
+
+
+function showWinScreen() {
+  document.querySelector('#win-section').classList.remove('hidden');
+}
+
+
+/**
  * Initialize the application in the window load event.
  */
- window.addEventListener("load", () => new App());
+ window.addEventListener("load", initPage);
